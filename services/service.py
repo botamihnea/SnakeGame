29b2c93a89089ac3e025a00d0970035403c86e9b
botamihnea <mihneabota2004@gmail.com>
2024-01-29 13:32:36 +0200

    basic moves work

--- services/service.py
+++ services/service.py
@@ -23,9 +23,96 @@ class Service:
                 self.__board.set_square(i, j, "a")
                 apples -= 1
 
-    def move(self, steps):
-        for i in range(steps):
-            size = self.__board.get_size()
-            self.__board.set_square(size // 2, size // 2, "+")
-            self.__board.set_square(size // 2 - 1, size // 2, "*")
-            self.__board.set_square(size // 2 + 1, size // 2, "+")
+    def change_direction(self, direction):
+
+        size = self.__board.get_size()
+        pos_i = 0
+        pos_j = 0
+        counter = 0
+        for i in range(size):
+            for j in range(size):
+                if self.__board.get_square(i, j) == "*":
+                    pos_i = i
+                    pos_j = j
+
+        if direction == "up":
+            for i in range(size):
+                for j in range(size):
+                    if self.__board.get_square(i, j) == "+":
+                        self.__board.set_square(i, j, "")
+                        counter += 1
+
+            self.__board.set_square(pos_i - 1, pos_j, "*")
+            self.__board.set_square(pos_i, pos_j, "")
+            pos_i = pos_i - 1
+
+            for i in range(1, counter + 1):
+                self.__board.set_square(pos_i + i, pos_j, "+")
+
+        if direction == "down":
+            for i in range(size):
+                for j in range(size):
+                    if self.__board.get_square(i, j) == "+":
+                        self.__board.set_square(i, j, "")
+                        counter += 1
+            self.__board.set_square(pos_i + 1, pos_j, "*")
+            self.__board.set_square(pos_i, pos_j, "")
+
+            pos_i = pos_i + 1
+
+            for i in range(1, counter + 1):
+                self.__board.set_square(pos_i - i, pos_j, "+")
+
+        if direction == "left":
+            for i in range(size):
+                for j in range(size):
+                    if self.__board.get_square(i, j) == "+":
+                        self.__board.set_square(i, j, "")
+                        counter += 1
+            self.__board.set_square(pos_i, pos_j - 1, "*")
+            self.__board.set_square(pos_i, pos_j, "")
+
+            pos_j = pos_j - 1
+
+            for i in range(1, counter + 1):
+                self.__board.set_square(pos_i, pos_j + i, "+")
+
+    def move(self, steps, direction):
+        size = self.__board.get_size()
+        pos_i = 0
+        pos_j = 0
+        counter = 0
+        for i in range(size):
+            for j in range(size):
+                if self.__board.get_square(i, j) == "*":
+                    pos_i = i
+                    pos_j = j
+
+        if direction == "up":
+            for i in range(size):
+                for j in range(size):
+                    if self.__board.get_square(i, j) == "+":
+                        self.__board.set_square(i, j, "")
+                        counter += 1
+            self.__board.set_square(pos_i - steps, pos_j, "*")
+            self.__board.set_square(pos_i, pos_j, "")
+            pos_i = pos_i - steps
+
+            for i in range(1, counter + 1):
+                self.__board.set_square(pos_i + i, pos_j, "+")
+
+        elif direction == "down":
+            counter = 0
+            for i in range(size):
+                for j in range(size):
+                    if self.__board.get_square(i, j) == "+":
+                        self.__board.set_square(i, j, "")
+                        counter += 1
+            self.__board.set_square(pos_i + steps, pos_j, "*")
+            self.__board.set_square(pos_i, pos_j, "")
+            pos_i = pos_i + steps
+
+            for i in range(1, counter + 1):
+                self.__board.set_square(pos_i - i, pos_j, "+")
+
+
--- ui/ui.py
+++ ui/ui.py
@@ -8,3 +8,33 @@ class UI:
 
     def run_ui(self):
         print(self.__board)
+        direction = "up"
+        while True:
+            try:
+                user_input = input("Enter command: ")
+                command = user_input.split(" ")
+                if command[0] == "move":
+                    if command == ";":
+                        steps = 1
+                    else: steps = int(command[1])
+                    self.__service.move(steps, direction)
+                elif command[0] == "up" and direction == "down":
+                    print("You cannot go 180 degrees!")
+                elif command[0] == "up":
+                    direction = "up"
+                    self.__service.change_direction(direction)
+                elif command[0] == "down" and direction == "up":
+                    print("You cannot go 180 degrees!")
+                elif command[0] == "down":
+                    direction = "down"
+                    self.__service.change_direction(direction)
+                elif command[0] == "left" and direction == "right":
+                    print("You cannot go 180 degrees!")
+                elif command[0] == "left":
+                    direction = "left"
+                    self.__service.change_direction(direction)
+
+            except ValueError as ve:
+                print(ve)
+
+            print(self.__board)
